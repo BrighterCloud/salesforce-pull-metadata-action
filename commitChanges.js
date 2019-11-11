f124113d2645905078c488d2cff113d846fb8d65
Joschi Von Rehberg <vonrehberg88@gmail.com>
2019-11-11 14:55:41 +0100

    we only need to checkin files once as we don't have intermediate steps

--- commitChanges.js
+++ commitChanges.js
@@ -26,6 +26,22 @@ allChanges.sort(function(a, b) {
     }
 });
 
+function removeDuplicates(originalArray, prop1, prop2) {
+    var newArray = [];
+    var lookupObject  = {};
+
+    for(var i in originalArray) {
+       lookupObject[originalArray[i][prop1 + "-" + prop2]] = originalArray[i];
+    }
+
+    for(i in lookupObject) {
+        newArray.push(lookupObject[i]);
+    }
+     return newArray;
+}
+
+allChanges = removeDuplicates(allChanges, "type", "Name");
+
 function getPathFromType(type) {
     switch(type) {
         case "ApexClass":
