cdc2f4f86f54d20cfceefa0ff93ab81874d60b3c
Joschi Von Rehberg <vonrehberg88@gmail.com>
2019-11-13 17:31:28 +0100

    walking directory without exit

--- commitChanges.js
+++ commitChanges.js
@@ -99,22 +99,23 @@ var walk = function(dir, done) {
     var results = [];
     fs.readdir(dir, function(err, list) {
       if (err) return done(err);
-      var pending = list.length;
-      if (!pending) return done(null, results);
-      list.forEach(function(file) {
+      var i = 0;
+      (function next() {
+        var file = list[i++];
+        if (!file) return done(null, results);
         file = path.resolve(dir, file);
         fs.stat(file, function(err, stat) {
           if (stat && stat.isDirectory()) {
             walk(file, function(err, res) {
               results = results.concat(res);
-              if (!--pending) done(null, results);
+              next();
             });
           } else {
             results.push(file);
-            if (!--pending) done(null, results);
+            next();
           }
         });
-      });
+      })();
     });
   };
 
@@ -137,12 +138,13 @@ async function commitChanges() {
             break;
         }
     }
-    process.exit(0);
+    // process.exit(0);
 }
 
 walk("/github/workspace", function(err, results) {
+    console.log("done walking");
     if (err) throw err;
     console.log(results);
-  });
+});
 
 commitChanges();
