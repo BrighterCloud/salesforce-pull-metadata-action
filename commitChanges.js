2a1bb362d52917e7cc887c428c123c90c20626af
Christian Jeschke <vonrehberg88@gmail.com>
2021-01-07 13:00:50 +0100

    fixing territory model and type

--- commitChanges.js
+++ commitChanges.js
@@ -26,12 +26,14 @@ var changesTerritory2Model = JSON.parse(fs.readFileSync("changesTerritory2Model.
 if (changesTerritory2Model.length > 1) {
     changesTerritory2Model = changesTerritory2Model.result.map(function(change) { change.Name = change.fullName; change.type = "Territory2Model"; change.LastModifiedBy = change.lastModifiedByName; change.LastModifiedDate = change.lastModifiedDate; return change; });
 } else {
+    changesTerritory2Model.Name = changesTerritory2Model.fullName; changesTerritory2Model.type = "Territory2Model"; changesTerritory2Model.LastModifiedBy = changesTerritory2Model.lastModifiedByName; changesTerritory2Model.LastModifiedDate = changesTerritory2Model.lastModifiedDate;
     changesTerritory2Model = [changesTerritory2Model];
 }
 var changesTerritory2Type = JSON.parse(fs.readFileSync("changesTerritory2Type.json", "utf8"));
 if (changesTerritory2Type.length > 1) {
     changesTerritory2Type = changesTerritory2Type.result.map(function(change) { change.Name = change.fullName; change.type = "Territory2Type"; change.LastModifiedBy = change.lastModifiedByName; change.LastModifiedDate = change.lastModifiedDate; return change; });
 } else {
+    changesTerritory2Type.Name = changesTerritory2Type.fullName; changesTerritory2Type.type = "Territory2Type"; changesTerritory2Type.LastModifiedBy = changesTerritory2Type.lastModifiedByName; changesTerritory2Type.LastModifiedDate = changesTerritory2Type.lastModifiedDate;
     changesTerritory2Type = [changesTerritory2Type];
 }
 var changesTerritory2Rule = JSON.parse(fs.readFileSync("changesTerritory2Rule.json", "utf8")).result.map(function(change) { change.Name = change.fullName; change.type = "Territory2Rule"; change.LastModifiedBy = change.lastModifiedByName; change.LastModifiedDate = change.lastModifiedDate; return change; });
